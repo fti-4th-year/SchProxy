dcbbfe3f6dd866e623b5e5a942b0e25bdf0ab2f0
prostomen <antonshreyner@gmail.com>
2015-09-24 20:27:50 +0600

    working

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,93 @@
 #include <list>
 #include <tcp/SocketFactory.h>
 #include <tcp/SocketListener.h>
+#include <sys/signal.h>
+#include <tcp/Socks4Manager.h>
 template< typename T , typename P >
 void operator-=( std::list< T > &out , P i )
 {
 	out.erase( i );
 }
+template< typename T >
+void operator+=( std::list< T > &out , T const &i )
+{
+	out.push_back( i );
+}
+void sigpipeHndl( int sign )
+{
+
+}
+struct Pipe
+{
+	SocketHandler in;
+	SocketHandler out;
+	void doWork()
+	{
+		Msg tmp;
+		try
+		{
+			in.timeout( 100 ) >> tmp;
+		} catch( TimeoutException const &e )
+		{
+		}
+		if( tmp.size() != 0 )
+		{
+			//std::cout << tmp.getString() << "\n";
+			out << tmp;
+			tmp = "";
+		}
+		try
+		{
+			out.timeout( 100 ) >> tmp;
+		} catch( TimeoutException const &e )
+		{
+		}
+		if( tmp.size() != 0 )
+		{
+			//std::cout << tmp.getString() << "\n";
+			in << tmp;
+			tmp = "";
+		}
+	}
+
+};
 int main( int argc , char *argv[] )
 {
-	/*SocketHandler s( Socket( "httpbin.org" , 80 ) );
-	 s << HTTPRequestFactory::simpleGET( "httpbin.org" , "/ip" );
-	 std::cout << s.timeout( 1000 ) << "\n";*/
-	SocketFactory factory( SocketListener( 8080 ) );
-	std::list< SocketHandler > sockets;
+	signal( SIGPIPE , sigpipeHndl );
+	SocketFactory factory( SocketListener( atoi( argv[ 1 ] ) ) );
+	std::list< Pipe > pipes;
 	while( true )
 	{
-		maybe( sockets += factory.timeout( 100 ); );
-		all_over: iterate( p , sockets )
+		try
+		{
+			SocketHandler in( factory.timeout( 100 ).get() );
+			Msg socks4_request;
+			in >> socks4_request;
+			//std::cout << "request sent: " + Socks4Manager::getInfo( socks4_request.getString() ) << "\n";
+			SocketHandler out = Socks4Manager::getSocket( socks4_request );
+			std::cout << "connection created:" << out.getInfo() << "\n";
+			in << Socks4Manager::getSuccess();
+			pipes += Pipe{ in , out };
+		} catch( TimeoutException const &e )
+		{
+		} catch( TCPException const &e )
+		{
+			std::cout << e.getMsg() << "\n";
+		}
+		all_over: iterate( p , pipes )
 		{
 			try
 			{
-				maybe( std::cout << p->timeout( 100 ) << "\n"; *p << "test"; );
-			} catch( std::exception const &e )
+				p->doWork();
+			} catch( SocketCloseException const &e )
 			{
-				//std::cout << sockets.size() << e.what() << "\n";
-				sockets -= p;
+				std::cout << e.getMsg() << "\n";
+				pipes -= p;
+				goto all_over;
+			} catch( TCPException const &e )
+			{
+				std::cout << e.getMsg() << "\n";
+				pipes -= p;
 				goto all_over;
 			}
 		}
