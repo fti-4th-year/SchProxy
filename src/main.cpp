7b7eeb28ece463d7d3365d5a26a28123afcd4f07
kokoronomagnet <antonshreyner@gmail.com>
2015-10-08 00:06:09 +0600

    waiter

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 template< typename T , typename P >
 void operator-=( std::list< T > &out , P i )
 {
-	out.erase( i );
+	out.remove( i );
 }
 template< typename T >
 void operator+=( std::list< T > &out , T const &i )
@@ -26,55 +26,82 @@ int main( int argc , char *argv[] )
 	SocketFactory factory( SocketListener( atoi( argv[ 1 ] ) ) );
 	struct Pipe
 	{
+		int i;
 		SocketHandler in;
 		SocketHandler out;
+		bool operator==( Pipe const &p ) const
+		{
+			return p.i == i;
+		}
 	};
-	std::list< Pipe > pipes;
-	while( true )
+	Waiter waiter;
+	bool working = true;
+	waiter.pushSleepy( factory.getSocketListener( ) , [ &waiter , &factory , &working ]()
 	{
 		try
 		{
-			SocketHandler in( factory.timeout( 10 ).get() );
+			SocketHandler in( factory.get( ) );
 			Msg socks4_request;
 			in >> socks4_request;
-			//std::cout << "request sent: " + Socks4Manager::getInfo( socks4_request.getString() ) << "\n";
+			std::cout << "request sent: " + Socks4Manager::getInfo( socks4_request.getString() ) << "\n";
 			SocketHandler out = Socks4Manager::getSocket( socks4_request );
-			std::cout << "connection created:" << out.getInfo() << "\n";
-			in << Socks4Manager::getSuccess();
-			pipes += Pipe { in , out };
-		} catch( TimeoutException const &e )
+			std::cout << "connection created:" << out.getInfo( ) << "\n";
+			in << Socks4Manager::getSuccess( );
+			waiter.pushSleepy( in.getSocket() , [&waiter , in , out]()
+					{
+						try
+						{
+							try
+							{
+								in.timeout( 100 ) >> out;
+								//std::cout << "in woke up" << "\n";
+							} catch( TimeoutException const &e )
+							{
+
+							}
+						} catch( std::exception const &e )
+						{
+							std::cerr << "in socket:" << e.what( ) << "\n";
+							waiter.removeSleepy( in.getSocket() );
+							waiter.removeSleepy( out.getSocket() );
+						}
+					}
+			);
+			waiter.pushSleepy( out.getSocket() , [&waiter , in , out]()
+					{
+						try
+						{
+							try
+							{
+								out.timeout( 100 ) >> in;
+								//std::cout << "out woke up" << "\n";
+							} catch( TimeoutException const &e )
+							{
+
+							}
+						} catch( std::exception const &e )
+						{
+							std::cerr << "out socket:" << e.what( ) << "\n";
+							waiter.removeSleepy( in.getSocket() );
+							waiter.removeSleepy( out.getSocket() );
+						}
+					}
+			);
+		} catch( std::exception const &e )
 		{
-		} catch( TCPException const &e )
-		{
-			std::cout << e.getMsg() << "\n";
+			std::cerr << "main catcher:" << e.what() << "\n";
+			//working = false;
 		}
-		all_over: iterate( p , pipes )
+	} );
+	while( working )
+	{
+		try
 		{
-			try
-			{
-				try
-				{
-					p->in.timeout( 500 ) >> p->out;
-				} catch( TimeoutException const &e )
-				{
-				}
-				try
-				{
-					p->out.timeout( 500 ) >> p->in;
-				} catch( TimeoutException const &e )
-				{
-				}
-			} catch( SocketCloseException const &e )
-			{
-				std::cout << e.getMsg() << "\n";
-				pipes -= p;
-				goto all_over;
-			} catch( TCPException const &e )
-			{
-				std::cout << e.getMsg() << "\n";
-				pipes -= p;
-				goto all_over;
-			}
+			waiter.wait( 100 );
+		} catch( std::exception const &e )
+		{
+			std::cerr << "waiter catcher:" << e.what( ) << "\n";
+			working = false;
 		}
 	}
 	return 0;
